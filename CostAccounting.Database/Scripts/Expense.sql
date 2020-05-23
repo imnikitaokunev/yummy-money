6e9173ca8f78fc256143bf9cd762897408448307
nikitkasss <nikitosinos1@gmail.com>
2020-05-23 22:20:58 +0300

    feat: added database

---
+++ CostAccounting.Database/Scripts/Expense.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO [dbo].[Expense] ([CategoryId], [Amount], [Date], [Description])
+VALUES
+	(1, 5, GETDATE(), N'Шава'),
+	(2, 100, GETDATE(), N'Штоники'),
+	(3, 7, GETDATE(), N'Гель для душа'),
+	(3, 10, GETDATE(), N'Прочее')
