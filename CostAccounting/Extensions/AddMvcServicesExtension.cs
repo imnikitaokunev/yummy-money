a592e6db368223010986f542c08852440559f9f9
nikitkasss <nikitosinos1@gmail.com>
2020-05-28 00:02:51 +0300

    feat: added extension methods for the services collection

---
+++ CostAccounting/Extensions/AddMvcServicesExtension.cs
@@ -0,0 +1,15 @@
+﻿using CostAccounting.Services.Services;
+using CostAccounting.Web.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CostAccounting.Web.Extensions
+{
+    public static class AddMvcServicesExtension
+    {
+        public static void AddMvcServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ICategoryService, CategoryService>();
+        }
+    }
+}
