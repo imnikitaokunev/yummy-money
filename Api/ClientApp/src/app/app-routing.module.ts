f040f54697832fa8769d910f188bfdf82d3e2e92
nikitkasss <nikitosinos1@gmail.com>
2021-07-22 23:28:01 +0300

    fix: 404 on refresh fix

--- Api/ClientApp/src/app/app-routing.module.ts
+++ Api/ClientApp/src/app/app-routing.module.ts
@@ -2,25 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
-    loadChildren: () =>
-      import('./modules/home/home.module').then((m) => m.HomeModule),
-  },
-  {
-    path: 'transactions',
-    loadChildren: () =>
-      import('./modules/transactions/transactions.module').then((m) => m.TransactionsModule),
-  },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        loadChildren: () =>
+            import('./modules/home/home.module').then((m) => m.HomeModule),
+    },
+    {
+        path: 'transactions',
+        loadChildren: () =>
+            import('./modules/transactions/transactions.module').then(
+                (m) => m.TransactionsModule
+            ),
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
