3b809fdff5809c05b41b9d821e9002dd33cc6d88
nikitkasss <nikitosinos1@gmail.com>
2021-08-31 11:23:24 +0300

    feat: updated to Angular 12

--- web/src/app/modules/home/pages/home/home.component.ts
+++ web/src/app/modules/home/pages/home/home.component.ts
@@ -1,8 +1,5 @@
 import { environment } from './../../../../../environments/environment';
-import { ApiHttpService } from './../../../../core/services/api-http.service';
-import { ApiEndpointsService } from './../../../../core/services/api-endpoints.service';
 import { Component, OnInit } from '@angular/core';
-import { version } from '../../../../../../package.json';
 
 @Component({
     selector: 'app-home',
@@ -10,7 +7,6 @@ import { version } from '../../../../../../package.json';
 })
 export class HomeComponent implements OnInit {
     public environment = environment;
-    public version: string = version;
 
     constructor() {}
 
